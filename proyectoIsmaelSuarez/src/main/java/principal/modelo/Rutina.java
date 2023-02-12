74b0cd19d8692e423d33f041bd588643aa5a6658
unknown <ismaelsrz03@gmail.com>
2023-02-12 19:40:24 +0100

    Tabla rutina con sus atributos y relaciones

--- proyectoIsmaelSuarez/src/main/java/principal/modelo/Rutina.java
+++ proyectoIsmaelSuarez/src/main/java/principal/modelo/Rutina.java
@@ -1,5 +1,105 @@
 package principal.modelo;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import principal.modelo.Ejercicio;
+
+@Entity
+@Table(name="Rutinas")
 public class Rutina {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private Integer id;
+	
+	@Column(name="Nombre")
+	private String nombre;
+	
+	@ManyToMany(mappedBy="rutinas", fetch = FetchType.EAGER)
+	private Set<Ejercicio> ejercicios;
+	
+	@ManyToMany
+	@JoinTable(
+			name = "asigna",
+			joinColumns = {@JoinColumn(name = "id_Rutina")},
+			inverseJoinColumns = {@JoinColumn(name = "id_Entrenador")}
+			
+			)
+	private Set<Entrenador> entrenadores;
+	
+	@ManyToMany
+	@JoinTable(
+			name = "recibe",
+			joinColumns = {@JoinColumn(name = "id_Rutina")},
+			inverseJoinColumns = {@JoinColumn(name = "id_Alumno")}
+			
+			)
+	private Set<Alumno> alumnos;
+	
+	public Rutina() {
+		
+	}
+	
+	public Rutina(String n) {
+		nombre = n;
+		ejercicios = new HashSet<Ejercicio>();
+		entrenadores = new HashSet<Entrenador>();
+		alumnos = new HashSet<Alumno>();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Set<Ejercicio> getEjercicios() {
+		return ejercicios;
+	}
+
+	public void setEjercicios(Set<Ejercicio> ejercicios) {
+		this.ejercicios = ejercicios;
+	}
+
+	public Set<Entrenador> getEntrenadores() {
+		return entrenadores;
+	}
+
+	public void setEntrenadores(Set<Entrenador> entrenadores) {
+		this.entrenadores = entrenadores;
+	}
+
+	public Set<Alumno> getAlumnos() {
+		return alumnos;
+	}
+
+	public void setAlumnos(Set<Alumno> alumnos) {
+		this.alumnos = alumnos;
+	}
+	
+	
 
 }
